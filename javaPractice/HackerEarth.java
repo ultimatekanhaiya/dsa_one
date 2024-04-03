130e85ed5c203508d5c09c82c3d7c8e12d875a64
Kanhaiya Sharma <ultimatekanhaiya@gmail.com>
2024-04-03 17:28:19 +0530

    Deleted javaPractice directory

--- javaPractice/HackerEarth.java
+++
@@ -1,9 +0,0 @@
-public class HackerEarth {
-    int val = 10;
-
-    public static void main(String[] args) {
-        HackerEarth obj = new HackerEarth();
-        System.out.println(obj.val);
-    }
-
-}
